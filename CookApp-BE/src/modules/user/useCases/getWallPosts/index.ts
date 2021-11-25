7c387d4b626c07dd71d11b58327ea2aedd327c28
minhduc <minhduc9111999@gmail.com>
2021-11-26 01:58:08 +0700

    fix get post image url

--- CookApp-BE/src/modules/user/useCases/getWallPosts/index.ts
+++ CookApp-BE/src/modules/user/useCases/getWallPosts/index.ts
@@ -5,6 +5,7 @@ import { PageMetadata } from "base/dtos/pageMetadata.dto";
 import { PageOptionsDto } from "base/pageOptions.base";
 import { UserDTO } from "dtos/user.dto";
 import { IWallRepository } from "modules/user/adapters/out/repositories/wall.repository";
+import { ConfigService } from "nestjs-config";
 import { GetWallPostsResponse } from "./getWallPostsResponse";
 export class GetWallPostsQuery extends BaseQuery {
   queryOptions: PageOptionsDto;
@@ -19,11 +20,17 @@ export class GetWallPostsQueryHandler
   implements IQueryHandler<GetWallPostsQuery> {
   constructor(
     @Inject("IWallRepository")
-    private _wallRepo: IWallRepository
+    private _wallRepo: IWallRepository,
+    private _configService: ConfigService
   ) {}
   async execute(query: GetWallPostsQuery): Promise<GetWallPostsResponse> {
     const { queryOptions, user } = query;
     const posts = await this._wallRepo.getPosts(user, queryOptions);
+    posts.forEach((post) => {
+      post.images = post.images?.map(
+        (image) => this._configService.get("storage.publicUrl") + image
+      );
+    });
     const totalCount = await this._wallRepo.getTotalPosts(user);
     let meta: PageMetadata;
     if (posts.length > 0) {
