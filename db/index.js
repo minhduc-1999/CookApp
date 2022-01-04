edb83d325cae96729581b30354c6f7c5d2db086d
minhduc <minhduc9111999@gmail.com>
2022-01-05 05:08:40 +0700

    migrate data to atlas

--- db/index.js
+++ db/index.js
@@ -2,16 +2,16 @@ const { MongoClient } = require('mongodb');
 const fs = require('fs')
 
 
-const url = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'; //change this connectionString
+const url = 'mongodb+srv://tastify-be:NPcZ69Jk8aVY7wuG@cluster0.auwps.mongodb.net/cookapp?retryWrites=true&w=majority'; //change this connectionString
 const client = new MongoClient(url);
 
 // Config here
 const dbName = 'cookapp'
 const collectionName = 'foods'
-const fileName = 'food_1.json'
+const fileName = 'food_2.json'
 //
 
-async function main(filename) {
+async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
